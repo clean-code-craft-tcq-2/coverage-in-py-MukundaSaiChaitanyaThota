36e667bc6aed35cb6258bc7ed8b87bf1308d81ef
MukundaSaiChaitanyaThota <91048236+MukundaSaiChaitanyaThota@users.noreply.github.com>
2022-04-04 08:42:39 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -9,9 +9,9 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.infer_breach(80, 50, 100) == 'NORMAL')
     
   def test_classify_temperature_breach_for_PASSIVE_COOLING(self):
-    self.assertTrue(typewise_alert.coolingtype_range('PASSIVE_COOLING') == {'lowerLimit' : 0, 'upperLimit' : 35})
-    self.assertTrue(typewise_alert.coolingtype_range('HI_ACTIVE_COOLING') == {'lowerLimit' : 0, 'upperLimit' : 45})
-    self.assertTrue(typewise_alert.coolingtype_range('MED_ACTIVE_COOLING') == {'lowerLimit' : 0, 'upperLimit' : 40})
+    self.assertTrue(typewise_alert.classify_temperature_breach('PASSIVE_COOLING') == {'lowerLimit' : 0, 'upperLimit' : 35})
+    self.assertTrue(typewise_alert.classify_temperature_breach('HI_ACTIVE_COOLING') == {'lowerLimit' : 0, 'upperLimit' : 45})
+    self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING') == {'lowerLimit' : 0, 'upperLimit' : 40})
 
 
 if __name__ == '__main__':
