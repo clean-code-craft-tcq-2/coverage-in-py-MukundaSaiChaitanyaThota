4cc46d11bf1281b063e2d58594e48250354d4f69
MukundaSaiChaitanyaThota <91048236+MukundaSaiChaitanyaThota@users.noreply.github.com>
2022-04-04 08:30:15 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -8,17 +8,13 @@ def infer_breach(value, lowerLimit, upperLimit):
 
 
 def classify_temperature_breach(coolingType, temperatureInC):
-  lowerLimit = 0
-  upperLimit = 0
-  if coolingType == 'PASSIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 35
-  elif coolingType == 'HI_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 45
-  elif coolingType == 'MED_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 40
+  coolinglimit={
+    'PASSIVE_COOLING':{'lowerLimit':0,'upperLimit':35},
+    'HI_ACTIVE_COOLING':{'lowerLimit':0,'upperLimit':45},
+    'MED_ACTIVE_COOLING':{'lowerLimit':0,'upperLimit':40}
+  }
+  lowerLimit = coolinglimit[coolingType]['lowerLimit']
+  upperLimit = limit_defn[coolingType]['upperLimit']
   return infer_breach(temperatureInC, lowerLimit, upperLimit)
 
 
