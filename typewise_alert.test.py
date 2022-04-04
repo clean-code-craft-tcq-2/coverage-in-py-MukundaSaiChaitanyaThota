9da9293bcfad77e4277bf55277edb70a9161adb4
MukundaSaiChaitanyaThota <91048236+MukundaSaiChaitanyaThota@users.noreply.github.com>
2022-04-04 09:47:51 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -36,25 +36,25 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.send_to_email('TOO_HIGH') ==f'To: a.b@c.com, Hi, the temperature is too high')  
  
   def test_check_and_alert_send_to_email(self):
-    self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL','PASSIVE_COOLING',-20) == f'To: a.b@c.com, Hi, the temperature is too low')
+    self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL','PASSIVE_COOLING',-20)==f'To: a.b@c.com, Hi, the temperature is too low')
     self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL','PASSIVE_COOLING', 70)==f'To: a.b@c.com, Hi, the temperature is too high')
     self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'HI_ACTIVE_COOLING',-25)==f'To: a.b@c.com, Hi, the temperature is too low')
     self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'HI_ACTIVE_COOLING', 100)==f'To: a.b@c.com, Hi, the temperature is too high') 
     self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'MED_ACTIVE_COOLING', -35)==f'To: a.b@c.com, Hi, the temperature is too low')     
     self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'MED_ACTIVE_COOLING', 120)==f'To: a.b@c.com, Hi, the temperature is too high')   
-    self.assertFalse(typewise_alert.check_and_alert('NA', 'MED_ACTIVE_COOLING',30) == f'To: a.b@c.com, Not applicable')
+    self.assertFalse(typewise_alert.check_and_alert('NA', 'MED_ACTIVE_COOLING',30)==f'To: a.b@c.com, Not applicable')
     
   def test_check_and_alert_to_controller(self):
-    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'HI_ACTIVE_COOLING',-40) == f'{0xfeed}, TOO_LOW')
-    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'HI_ACTIVE_COOLING', 130) == f'{0xfeed}, TOO_HIGH')
-    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'HI_ACTIVE_COOLING', 35) == f'{0xfeed}, NORMAL')
-    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER','PASSIVE_COOLING',-10) == f'{0xfeed}, TOO_LOW')
-    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER','PASSIVE_COOLING', 90) == f'{0xfeed}, TOO_HIGH')
-    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER','PASSIVE_COOLING', 15) == f'{0xfeed}, NORMAL')
-    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'MED_ACTIVE_COOLING', -25) == f'{0xfeed}, TOO_LOW')
-    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'MED_ACTIVE_COOLING', 100) == f'{0xfeed}, TOO_HIGH')
-    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'MED_ACTIVE_COOLING',30) == f'{0xfeed}, NORMAL')  
-    self.assertFalse(typewise_alert.check_and_alert('NA', 'MED_ACTIVE_COOLING',25) == f'{0xfeed}, Not applicable')
+    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'HI_ACTIVE_COOLING',-40)==f'{0xfeed}, TOO_LOW')
+    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'HI_ACTIVE_COOLING', 130)==f'{0xfeed}, TOO_HIGH')
+    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'HI_ACTIVE_COOLING', 35)==f'{0xfeed}, NORMAL')
+    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER','PASSIVE_COOLING',-10)==f'{0xfeed}, TOO_LOW')
+    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER','PASSIVE_COOLING', 90)==f'{0xfeed}, TOO_HIGH')
+    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER','PASSIVE_COOLING', 15)==f'{0xfeed}, NORMAL')
+    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'MED_ACTIVE_COOLING', -25)==f'{0xfeed}, TOO_LOW')
+    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'MED_ACTIVE_COOLING', 100)==f'{0xfeed}, TOO_HIGH')
+    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'MED_ACTIVE_COOLING',30)==f'{0xfeed}, NORMAL')  
+    self.assertFalse(typewise_alert.check_and_alert('NA', 'MED_ACTIVE_COOLING',25)==f'{0xfeed}, Not applicable')
     
     
 if __name__ == '__main__':
