4b00d21a939702e7ec1c75caf8479d6e0a81c0da
MukundaSaiChaitanyaThota <91048236+MukundaSaiChaitanyaThota@users.noreply.github.com>
2022-04-04 10:06:36 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -21,9 +21,9 @@ def classify_temperature_breach(coolingType, temperatureInC):
 def check_and_alert(alertTarget, batteryChar, temperatureInC):
   breachType =classify_temperature_breach(batteryChar, temperatureInC)
   if alertTarget == 'TO_CONTROLLER':
-    alertMessage=send_to_controller(breachType)
+    alertMessage = send_to_controller(breachType)
   elif alertTarget == 'TO_EMAIL':
-    alertMessage=send_to_email(breachType)
+    alertMessage = send_to_email(breachType)
   else:
     alertMessage="Not applicable"
   return alertMessage
@@ -39,11 +39,11 @@ def send_to_email(breachType):
   if breachType == 'TOO_LOW':
     print(f'To: {recepient}')
     print('Hi, the temperature is too low')
-    return(f'To: {recepient}','Hi, the temperature is too low')
+    return(f'To: {recepient}, Hi, the temperature is too low')
   elif breachType == 'TOO_HIGH':
     print(f'To: {recepient}')
     print('Hi, the temperature is too high')
-    return(f'To: {recepient}','Hi, the temperature is too high')
+    return(f'To: {recepient}, Hi, the temperature is too high')
   else:
     return(f'To: {recepient}','Not applicable')
 
