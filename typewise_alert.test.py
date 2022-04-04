e309bf0d49cc5d937311d48d860d3c8162fb0768
MukundaSaiChaitanyaThota <91048236+MukundaSaiChaitanyaThota@users.noreply.github.com>
2022-04-04 10:08:22 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -33,7 +33,7 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.send_to_email('TOO_LOW') ==f'To: a.b@c.com, Hi, the temperature is too low')
     
   def test_send_to_email_TOO_HIGH(self):
-    self.assertTrue(typewise_alert.send_to_email('TOO_HIGH') ==f'To: a.b@c.com,Hi, the temperature is too high') 
+    self.assertTrue(typewise_alert.send_to_email('TOO_HIGH') ==f'To: a.b@c.com, Hi, the temperature is too high') 
 
   def test_check_and_alert_send_to_email(self):
     self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL','PASSIVE_COOLING',-20) == f'To: a.b@c.com, Hi, the temperature is too low')
